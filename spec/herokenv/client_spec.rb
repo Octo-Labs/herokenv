10f0c7215bf607e38927dab18e26111b7d8befaa
Jeremy Green <jeremy@octolabs.com>
2021-06-24 21:40:36 -0500

    Some slightly improved testing

--- spec/herokenv/client_spec.rb
+++ spec/herokenv/client_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe Herokenv::Client do
   let(:heroku_config) do
     {
       "HEROKU_TEST_VAR" => "test",
-      "HEROKU_TEST_VAR2" => "test2"
+      "HEROKU_TEST_VAR2" => "test2",
+      "HEROKU_TEST_VAR3" => "test3"
     }
   end
   let(:herokenv){ Herokenv::Client.new oauth_token: token, app: app }
@@ -16,6 +17,7 @@ RSpec.describe Herokenv::Client do
       expect(ENV['HEROKU_TEST_VAR']).to be_nil
     end
     allow(PlatformAPI).to receive(:connect_oauth).and_return(fake_platoform_api)
+    allow(fake_platoform_api).to receive_message_chain(:config_var, :info_for_app).and_return(heroku_config)
   end
 
   after do
@@ -27,30 +29,24 @@ RSpec.describe Herokenv::Client do
 
   describe 'populate_env_from' do
     it 'pulls config vars from a heroku app and populates the local ENV' do
-      expect(ENV['HEROKU_TEST_VAR']).to be_nil
-      expect(ENV['HEROKU_TEST_VAR2']).to be_nil
-      expect(fake_platoform_api).to receive_message_chain(:config_var, :info_for_app).and_return(heroku_config)
       herokenv.populate_env_from_app
       expect(ENV['HEROKU_TEST_VAR']).to eq('test')
       expect(ENV['HEROKU_TEST_VAR2']).to eq('test2')
+      expect(ENV['HEROKU_TEST_VAR3']).to eq('test3')
     end
 
     it 'takes an only: option' do
-      expect(ENV['HEROKU_TEST_VAR']).to be_nil
-      expect(ENV['HEROKU_TEST_VAR2']).to be_nil
-      expect(fake_platoform_api).to receive_message_chain(:config_var, :info_for_app).and_return(heroku_config)
       herokenv.populate_env_from_app only: ['HEROKU_TEST_VAR']
       expect(ENV['HEROKU_TEST_VAR']).to eq('test')
       expect(ENV['HEROKU_TEST_VAR2']).to be_nil
+      expect(ENV['HEROKU_TEST_VAR3']).to be_nil
     end
 
     it 'takes an except: option' do
-      expect(ENV['HEROKU_TEST_VAR']).to be_nil
-      expect(ENV['HEROKU_TEST_VAR2']).to be_nil
-      expect(fake_platoform_api).to receive_message_chain(:config_var, :info_for_app).and_return(heroku_config)
       herokenv.populate_env_from_app except: ['HEROKU_TEST_VAR2']
       expect(ENV['HEROKU_TEST_VAR']).to eq('test')
       expect(ENV['HEROKU_TEST_VAR2']).to be_nil
+      expect(ENV['HEROKU_TEST_VAR3']).to eq('test3')
     end
   end
 
